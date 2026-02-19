4010d58a1f030bd5126b363f14f170eed71e3f9f
Aansa-123 <aansarana786@gmail.com>
2026-02-19 15:28:36 +0500

    make version 3.0.0 in docker image

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send([
-    {'message': 'Hello, World!'},
-    {'message': 'This is a simple Express server running in a Docker container.'},
-    {'message': 'Feel free to modify the code and add more routes!'},
-    {'message': 'version: 2.0.0'}
-  ].join('\n'));
+res.send({
+'message': 'Hello, World!',
+'info': 'This is a simple Express server running in a Docker container.',
+'notes': 'Feel free to modify the code and add more routes!',
+'version': '2.0.0'
+},{
+'message': 'Hello, World!',
+'info': 'This is a simple Express server running in a Docker container.',
+'notes': 'Feel free to modify the code and add more routes!',
+'version': '2.0.0'
+},
+{
+'message': 'Hello, World!',
+'info': 'This is a simple Express server running in a Docker container.',
+'notes': 'Feel free to modify the code and add more routes!',
+'version': '3.0.0'
+}
+);
+
  
 });
 
