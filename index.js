a9c0e5feb96773b33fe5f696646b6154df6dea76
Aansa-123 <aansarana786@gmail.com>
2026-02-19 15:21:43 +0500

    manage versions in docker images

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send([
+    {'message': 'Hello, World!'},
+    {'message': 'This is a simple Express server running in a Docker container.'},
+    {'message': 'Feel free to modify the code and add more routes!'},
+    {'message': 'version: 2.0.0'}
+  ].join('\n'));
+ 
 });
 
 app.listen(3000, () => {
